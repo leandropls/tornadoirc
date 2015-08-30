7b178179d22af014c00ccb0b187414d60b30f148
Leandro Lima <leandro@limaesilva.com.br>
2015-08-30 04:36:40 -0300

    data.messages: created message dict

---
+++ data/messages.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+messages = {
+    'RPL_WELCOME':
+        ('001', 'Welcome to the Internet Relay Network '
+                '%(nick)s!%(username)s@%(hostname)s'),
+    'RPL_YOURHOST':
+        ('002', 'Your host is %(servername)s, running version %(version)s'),
+    'RPL_CREATED':
+        ('003', 'This server was created %(date)s'),
+    'RPL_MYINFO':
+        ('004', '%(servername)s %(version)s %(usermodes)s %(channelmodes)s'),
+}
