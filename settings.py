3601395ddb70ca8ca72249e179c9231c1087c558
Leandro Lima <leandro@limaesilva.com.br>
2015-08-30 17:15:34 -0300

    settings: reorganized settings

--- settings.py
+++ settings.py
@@ -6,34 +6,25 @@
 process_name = 'tornadoircd'
 
 ##
+# Local time
+##
+import pytz, datetime
+localtime = pytz.timezone('America/Sao_Paulo')
+now = datetime.datetime.now(localtime)
+
+##
 # IRCd
 ##
+from datetime import datetime
 ircd = {
-    'servername': 'irc.testnetwork.org',
+    'name': 'irc.testnetwork.org',
+    'date': str(now),
     'listen': [('127.0.0.1', 6667)],
     'motd': ['Welcome to my humble server!',
              'This server is a test version of tornadoirc',
              'If you find any issues, please report to the developer.'],
-}
-
-##
-# User
-##
-user = {
     'nicklen': 30,
 }
-
-
-##
-# Local time
-##
-import pytz
-localtime = pytz.timezone('America/Sao_Paulo')
-
-##
-# Timestamp range (current value: years of 2015 to 2096)
-##
-timestamp_range = {'min': 1420077600, 'max': 4000000000}
 
 ##
 # Profiling
