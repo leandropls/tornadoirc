660199531a86b731d26aef6aa88f325f84673fe9
Leandro Lima <leandro@limaesilva.com.br>
2015-08-30 05:02:38 -0300

    data.messages: bug fix and added new messages

--- data/messages.py
+++ data/messages.py
@@ -2,12 +2,18 @@
 
 messages = {
     'RPL_WELCOME':
-        ('001', 'Welcome to the Internet Relay Network '
+        ('001', ':Welcome to the Internet Relay Network '
                 '%(nick)s!%(username)s@%(hostname)s'),
     'RPL_YOURHOST':
-        ('002', 'Your host is %(servername)s, running version %(version)s'),
+        ('002', ':Your host is %(servername)s, running version %(version)s'),
     'RPL_CREATED':
-        ('003', 'This server was created %(date)s'),
+        ('003', ':This server was created %(date)s'),
     'RPL_MYINFO':
-        ('004', '%(servername)s %(version)s %(usermodes)s %(channelmodes)s'),
+        ('004', ':%(servername)s %(version)s %(usermodes)s %(channelmodes)s'),
+    'ERR_NONICKNAMEGIVEN':
+        ('431', ':No nickname given'),
+    'ERR_NEEDMOREPARAMS':
+        ('461', '%(command)s :Not enough parameters'),
+    'ERR_ALREADYREGISTRED':
+        ('462', ':You may not reregister'),
 }
