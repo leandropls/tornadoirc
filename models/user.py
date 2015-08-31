615523f29290bb4dddbbc35b2ee78363c504955c
Leandro Lima <leandro@limaesilva.com.br>
2015-08-31 02:30:55 -0300

    models.user: bug fixes

--- models/user.py
+++ models/user.py
@@ -20,8 +20,11 @@ class User(object):
     realname = Undefined(Optional[str])
     connection = Undefined('Connection')
     server = Undefined(Server)
-    registered = Undefined(bool)
+    pingtimer = None
 
+    ##
+    # Nick getter and setter
+    ##
     _nick_regex = re.compile(r'(\w+)')
 
     @property
@@ -39,11 +42,21 @@ class User(object):
         nicklen = self.server.settings['nicklen']
         value = value[0 : min(len(value), nicklen)]
 
+        if value == self._nick:
+            return
+
         if value in self.server.users:
             raise NicknameInUseError(value)
 
         self._nick = value
 
+    @property
+    def address(self):
+        return '%s!%s@%s' % (self.nick, self.username, self.hostname)
+
+    ##
+    # Init
+    ##
     def __init__(self, nick: str, connection: 'Connection', server: Server,
                  hopcount: int, username: str, hostname: str, servername: str,
                  realname: str):
@@ -55,35 +68,21 @@ class User(object):
         self.hostname = hostname
         self.servername = servername
         self.realname = realname
-        self.registered = True
 
-        self.send_message('RPL_WELCOME',
-                          nick = self.nick,
-                          username = self.username,
-                          hostname = self.hostname)
-
-        self.send_message('RPL_YOURHOST',
-                          servername = self.server.name,
-                          version = self.server.version)
-
-        self.send_message('RPL_CREATED',
-                          date = self.server.date)
-
-        self.send_message('RPL_MYINFO',
-                          servername = self.server.name,
-                          date = self.server.date,
-                          version = self.server.version,
-                          usermodes = self.server.usermodes,
-                          channelmodes = self.server.channelmodes)
-
-        self.cmd_motd(None)
-
-        self.pingtimer = PeriodicCallback(self.send_ping, int(60e3))
+        self.pingtimer = PeriodicCallback(self.send_ping,
+            int(self.server.settings['pinginterval'] * 1e3))
         self.pingtimer.start()
 
         logger.info('Registered new user: %s!%s@%s',
                     self.nick, self.username, self.hostname)
 
+    def closed(self):
+        if self.pingtimer:
+            self.pingtimer.stop()
+
+    ##
+    # Server send msg commands
+    ##
     def send_message(self, *args, **kwargs):
         '''Send message to user.'''
         self.connection.send_message(*args, **kwargs)
@@ -94,13 +93,28 @@ class User(object):
         message = message.encode('utf-8')
         self.connection.stream.write(message)
 
+    def send_welcome(self):
+        self.send_message('RPL_WELCOME')
+        self.send_message('RPL_YOURHOST', version = self.server.version)
+        self.send_message('RPL_CREATED', date = self.server.date)
+        self.send_message('RPL_MYINFO',
+                          date = self.server.date,
+                          version = self.server.version,
+                          usermodes = self.server.usermodes,
+                          channelmodes = self.server.channelmodes)
+
+        self.cmd_motd(None)
+
+    ##
+    # Server command handlers
+    ##
     def cmd_motd(self, prefix: Optional[str], target: Optional[str] = None):
         '''Process MOTD command.'''
         if not self.server.settings['motd']:
             raise NoMotdError()
-        self.send_message('RPL_MOTDSTART', servername = self.server.name)
+        self.send_message('RPL_MOTDSTART')
         for text in self.server.settings['motd']:
-            self.send_message('RPL_MOTD', text = text)
+            self.send_message('RPL_MOTD', text = text[0 : min(len(text) + 1, 81)])
         self.send_message('RPL_ENDOFMOTD')
 
     def cmd_ping(self, prefix: Optional[str], payload: str,
@@ -109,3 +123,14 @@ class User(object):
         if destination and destination != self.server.name:
             raise NoSuchServerError(servername = destination)
         self.send_message('CMD_PONG', payload = payload)
+
+    def cmd_nick(self, prefix: Optional[str], nick: str):
+        '''Process NICK command.'''
+        oldaddr = self.address
+        oldnick = self.nick
+        self.nick = nick
+        if self.nick != oldnick:
+            self.server.users[self.nick] = self
+            del self.server.users[oldnick]
+
+        self.send_message('CMD_NICK', oldaddr = oldaddr, nick = self.nick)
